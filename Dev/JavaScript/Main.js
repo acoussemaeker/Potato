e3a132eff9fe8e54a8a43ece4314ac9b479a234c
Nicolas JANSSOONE <nicolas.janssoone@gmail.com>
2016-06-09 11:23:25 +0200

    Added demo code

--- Dev/JavaScript/Main.js
+++ Dev/JavaScript/Main.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+var logger = new LoggerObserver();
+
+var neurone = new Neurone();
+
+neurone.weights = [1,-5,42];
+
+neurone.transfert = input => input < 0.5 ? -1 : 1;
+neurone.observer = logger;
+
+var neuroneResult;
+
+try {
+	neuroneResult = neurone.execute([5,9,4]);
+}
+catch (exception) {
+	logger.notify(exception);
+}
